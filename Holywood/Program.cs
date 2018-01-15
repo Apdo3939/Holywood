de343ea6ae36fb1ca3c12fcad4132e92e245c1ae
Alexandre Pereira <apdo3939@hotmail.com>
2018-01-15 21:04:11 -0200

    Classe Artista Criada e Funcional

--- Holywood/Program.cs
+++ Holywood/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Holywood.dominio;
 
 namespace Holywood
 {
     class Program
     {
+        static List<Artista> artista = new List<Artista>();
+
         static void Main(string[] args)
         {
+            //variaveis
+            int opcao = 0;
+            
+            //entradas
+            artista.Add(new Artista(101, "Scarlett Johansson", 4000000.00));
+            artista.Add(new Artista(102, "Chris Evans       ", 2500000.00));
+            artista.Add(new Artista(103, "Robert Downey Jr. ", 3000000.00));
+            artista.Add(new Artista(104, "Morgan Freeman    ", 4000000.00));
+
+            //loop principal do programa
+            while (opcao != 5)
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("\t--------------------------------------------------------------");
+                Console.WriteLine();
+                Console.Write("\tPor favor Digite uma opção acima: ");
+                opcao = int.Parse(Console.ReadLine());
+                
+                for (int i = 0; i < artista.Count; i++)
+                {
+                    Console.WriteLine("\t" + artista[i]);
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ Holywood/dominio/Artista.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+
+namespace Holywood.dominio
+{
+    class Artista
+    {
+        //atributos
+        public int codigo { get; set; }
+        public string nome { get; set; }
+        public double cache { get; set; }
+
+        //construtor
+        public Artista(int codigo, string nome, double cache)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.cache = cache;
+
+        }
+        public override string ToString()
+        {
+            return  codigo
+                + ", \t"
+                + nome
+                + ", \t\tCachê: "
+                + cache.ToString("F2", CultureInfo.InvariantCulture); 
+        }
+
+    }
+}
